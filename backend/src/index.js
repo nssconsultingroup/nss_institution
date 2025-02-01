71f18ecf1b27553a92ba723889d0ea507f062793
Shikhar Srivastav <shikharsrivastav9450@gmail.com>
2025-02-01 21:10:10 +0530

    create a server and configure dotenv filein server

---
+++ backend/src/index.js
@@ -0,0 +1,8 @@
+const dotenv = require('dotenv').config();
+const app = require('./app')
+
+const port = process.env.PORT;
+app.listen(port, () => {
+    console.log("app is running on port: ", port);
+    
+})
